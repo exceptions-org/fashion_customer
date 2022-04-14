a7c22d0e99b1506792ca884891a3bf3b7e28758d
b3ingHassan <53341268+b3ingHassan@users.noreply.github.com>
2022-04-14 15:50:18 +0530

    created constants file

---
+++ lib/utils/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class KConstants {
+  final kPrimary100 = Color(0xff604FCD);
+  final kPrimary75 = Color(0xff887BD9);
+  final kTextfeildColor = Color(0xffAFA7E6);
+  final kBgColor = Color(0xffFAFAFF);
+  final txtColor100 = Color(0xff130B43);
+  final txtColor75 = Color(0xff4E4872);
+  final kBorderColor = Color(0xffC7D4EE);
+}
