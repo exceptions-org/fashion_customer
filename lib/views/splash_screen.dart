ef33dfc5fbbd2c68085241ae026cb925dc972b71
gms-sam <gms.sam844@gmail.com>
2022-04-30 02:55:09 +0530

    update

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -4,11 +4,13 @@ import 'package:fashion_customer/controller/cart_controller.dart';
 import 'package:fashion_customer/controller/controller.dart';
 import 'package:fashion_customer/main.dart';
 import 'package:fashion_customer/startup_page.dart';
+import 'package:fashion_customer/utils/constants.dart';
 import 'package:fashion_customer/utils/fcm_service.dart';
 import 'package:fashion_customer/views/login_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -28,13 +30,20 @@ class _SplashScreenState extends State<SplashScreen> with AfterLayoutMixin {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              Image.asset(
+                "Icons/FashioLogo.png",
+                height: 100,
+              ),
               Hero(
                 tag: 'title',
                 child: Material(
                   type: MaterialType.transparency,
                   child: Text(
                     "Fashio",
-                    style: TextStyle(color: Colors.purple, fontSize: 25),
+                    style: GoogleFonts.montserratAlternates(
+                      color: KConstants.kPrimary100,
+                      fontSize: 25,
+                    ),
                   ),
                 ),
               )
@@ -49,7 +58,7 @@ class _SplashScreenState extends State<SplashScreen> with AfterLayoutMixin {
     controller.setEmptyUser();
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     bool? isfirst = sharedPreferences.getBool("first");
-    await Future.delayed(Duration(seconds: 2));
+    await Future.delayed(Duration(seconds: 10));
     if (isfirst != true) {
       Navigator.pushAndRemoveUntil(context,
           CupertinoPageRoute(builder: (c) => StartupPage()), (route) => false);
@@ -58,10 +67,10 @@ class _SplashScreenState extends State<SplashScreen> with AfterLayoutMixin {
       getIt<CartController>().init();
       await controller.getUser();
       controller.saveAdminToken();
-      Navigator.pushAndRemoveUntil(
-          context,
-          CupertinoPageRoute(builder: (c) => BottomNavigation()),
-          (route) => false);
+      // Navigator.pushAndRemoveUntil(
+      //     context,
+      //     CupertinoPageRoute(builder: (c) => BottomNavigation()),
+      //     (route) => false);
     } else {
       Navigator.pushAndRemoveUntil(context,
           CupertinoPageRoute(builder: (c) => LoginPage()), (route) => false);
